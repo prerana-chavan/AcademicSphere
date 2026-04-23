7a8c80d9d9a9899f3fd241a4d4eb4cf6e7aeaedc
Prerana Chavan <preranac1884@gmail.com>
2026-04-23 20:44:39 +0530

    Initial commit - frontend + backend

---
+++ AcademicSphere-Backend/src/main/java/com/tka/academicsphere/api/AcademicSphereApplication.java
@@ -0,0 +1,13 @@
+package com.tka.academicsphere.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicSphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicSphereApplication.class, args);
+		System.err.println("app started...");
+	}
+}
